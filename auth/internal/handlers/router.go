451fbe9c757a2b1e9d56e0ed3265bc3bfbf0e8ab
GGenken <genken.gleb@gmail.com>
2025-07-21 17:10:06 +0300

    set auth microservice's responses' content types to JSON

--- auth/internal/handlers/router.go
+++ auth/internal/handlers/router.go
@@ -9,9 +9,29 @@ import (
 func InitializeRouter(db *gorm.DB) *http.ServeMux {
 	router := http.NewServeMux()
 
-	router.Handle("/ping", middleware.Apply(PingHandler(), middleware.WithDB(db)))
-	router.Handle("/register", middleware.Apply(RegisterHandler(), middleware.WithDB(db)))
-	router.Handle("/login", middleware.Apply(LoginHandler(), middleware.WithDB(db)))
+	router.Handle(
+		"/ping",
+		middleware.Apply(
+			PingHandler(),
+			middleware.WithDB(db),
+		),
+	)
+	router.Handle(
+		"/register",
+		middleware.Apply(
+			RegisterHandler(),
+			middleware.WithDB(db),
+			middleware.JSONContentType,
+		),
+	)
+	router.Handle(
+		"/login",
+		middleware.Apply(
+			LoginHandler(),
+			middleware.WithDB(db),
+			middleware.JSONContentType,
+		),
+	)
 
 	return router
 }
